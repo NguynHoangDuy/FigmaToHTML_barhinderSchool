748740f286f76246236d4979b1786031330e3e5c
NguynHoangDuy <nguyenhoangduy933@gmail.com>
2022-08-27 16:47:30 +0700

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+
+
+const btnToggle = document.querySelector(".header-top--toggle")
+const btnClose = document.querySelector(".header-top--close")
+const menuToggle = document.querySelector(".header-top--menu")
+btnToggle.addEventListener("click", ()=>{
+  menuToggle.classList.remove("menu-NotActive")
+  menuToggle.style = " transition: all 0.3s linear;"
+})
+btnClose.addEventListener("click", ()=>{
+  menuToggle.classList.add("menu-NotActive")
+  menuToggle.style = " transition: all 0.3s linear;"
+})
+
+const header = document.querySelector(".header-top")
